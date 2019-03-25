5eadeaa686e6ecac84b02943189017b34c0d77ef
zinovievpavel <36676505+zinovievpavel@users.noreply.github.com>
2019-03-25 22:53:13 +0300

    add some js + styling main
    Добавил какой то js скрипт который обрезает текст в превьюше статьи
    
    Добавил стили для превьюшек
    
    Добавил картинку
    
    И еще по мелочи

--- branch-1/assets/js/scripts.js
+++ branch-1/assets/js/scripts.js
@@ -1,7 +1,8 @@
-var size = 12,
-    postDescription= $(".post-description"),
-    postText = postDescription.text();
-
-if(postText.length > size){
-	postDescription.text(postText.slice(0, size) + '...');
+// Обрезаем кол-во текста в post-description по высоте
+var p=$('.post-description p');
+var divh=$('.post-description').height();
+while ($(p).outerHeight()>divh) {
+    $(p).text(function (index, text) {
+        return text.replace(/\W*\s(\S)*$/, '...');
+    });
 }
