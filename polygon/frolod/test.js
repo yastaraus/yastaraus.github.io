068507b7e0d9478c78bd805fb464618f4737fd53
Павел Зиновьев <pavelzinoviev@yastaraus-mbp.local>
2020-12-08 00:22:42 +0300

    bunch of new stuff

---
+++ polygon/frolod/test.js
@@ -0,0 +1,19 @@
+let firstName = 'Pavel';
+let lastName = 'Zinoviev';
+let fullName = '';
+
+function createFullName(firstName,lastName) {
+  fullname = firstName + ' ' + lastName;
+}
+
+let user = {
+  id = '';
+  firstName = ''
+  lastName = '';
+  fullName = '';
+  gender = '';
+  age = '';
+  birthday = '';
+  country = '';
+  city = '';
+}
