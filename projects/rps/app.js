4d597b7e0ecc539a9bd79b15e316f0295e51d0f7
yastaraus <48056887+yastaraus@users.noreply.github.com>
2020-12-20 20:57:00 +0300

    Local Storage

--- projects/rps/app.js
+++ projects/rps/app.js
@@ -1,24 +1,11 @@
-/*
-1. Добавить типы игр - классическая или от шелдона купера
-2. Добавить правила игры - по приколу
-*/
-
-// Потом можно сделать разные типы игр - классика и от шелдона. Как сделать так что бы в зависимости от выбора типа игры в массив набивалось разное кол-во значений?
-
-// Начальный экран с кнопкой "Начать"
-// После нажатия кнопки "Начать" сразу считается таймер, за время которого ты должен выбрать кнопку. Если не успеваешь то за тебя выбирает рандомайзер, на экране нужно что-то показать, типа "Панический выбор", что бы пользователь понял откуда это значение. Если успеваешь твой выбор сравнивается с компом. Если выйграл показывается "Ты выйграл", если нет то "Ты проиграл". 
-
-// const rock = document.getElementById("rock");
-// const paper = document.getElementById("paper");
-// const scissors = document.getElementById("scissors");
-
-let userScore = 0;
-let computerScore = 0;
-let userScoreElem = document.querySelector('.player_score');
+let userScoreElem = document.querySelector('.user_score');
 let computerScoreElem = document.querySelector('.computer_score');
 let gameResultElem = document.querySelector('.game_result');
-
+let userScore = 0;
+let computerScore = 0;
 let userChoice;
+
+readLocalScoreData();
 
 // Получаем все элементы с data-choice
 let allChoicesElements = document.querySelectorAll('[data-choice]');
@@ -29,33 +16,38 @@ for (let i = 0; i < allChoicesElements.length; i++) {
         userChoice = allChoicesElements[i].dataset.choice;
         game(userChoice);
     })
-}
+};
 
 // Получаем выбор компьютера (рандомный)
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * allChoicesElements.length);
     return allChoicesElements[randomChoice].dataset.choice;
-}
+};
 
+// При победе
 function win() {
-    gameResultElem.innerHTML = "You Win"
+    gameResultElem.innerHTML = "You Win";
     userScore++;
     userScoreElem.innerHTML = userScore;
+    writeLocalScoreData(userScore, computerScore);
 };
+
+// При поражении
 function lose() {
-    gameResultElem.innerHTML = "You Lost"
+    gameResultElem.innerHTML = "You Lost";
     computerScore++;
     computerScoreElem.innerHTML = computerScore;
+    writeLocalScoreData(userScore, computerScore);
 };
+
+// При ничье
 function draw() {
-    gameResultElem.innerHTML = "It's a Draw"
+    gameResultElem.innerHTML = "It's a Draw";
 };
 
 // Игра
 function game(user) {
-
     let computer = getComputerChoice();
-
     // Правила игры
     switch(user + " " + computer) {
         case "rock scissors":
@@ -74,4 +66,26 @@ function game(user) {
             draw();
             break;
     }
-}
+};
+
+// Пишем в local storage
+function writeLocalScoreData(localUserScore, localComputerScore) {
+    localStorage.setItem('user_score', localUserScore);
+    localStorage.setItem('computer_score', localComputerScore);
+};
+
+// Читаем из local storage
+function readLocalScoreData() {
+
+    if (localStorage.getItem('user_score') === null) {
+        localStorage.setItem('user_score', 0);
+    }
+    if (localStorage.getItem('computer_score') === null) {
+        localStorage.setItem('computer_score', 0);
+    }
+
+    userScore = localStorage.getItem('user_score');
+    computerScore = localStorage.getItem('computer_score');
+    userScoreElem.innerHTML = localStorage.getItem('user_score');
+    computerScoreElem.innerHTML = localStorage.getItem('computer_score');
+};
